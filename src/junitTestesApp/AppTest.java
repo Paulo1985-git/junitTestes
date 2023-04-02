19b67e2b4e80b609c7a86411932940a96dd38e0a
Paulo1985-git <paulomattos.vv@gmail.com>
2023-04-02 16:36:22 -0300

    Testes Junit implementados

--- src/junitTestesApp/AppTest.java
+++ src/junitTestesApp/AppTest.java
@@ -1,12 +1,38 @@
 package junitTestesApp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-class AppTest {
+import entities.Funcionario;
+
+class TestandoFuncionairo {
+	
+	Funcionario func = new Funcionario("Paulo", "11/02/1985", "059.411.417-93", "Lucia", "Brasileiro", 'M', "425", "01/01/2005", "Desenvolvedor", 5000.00);
 
 	@Test
-	void testandoSalarioMenorDoQue5000() {
-		
+	void testandoNome() {
+		assertEquals(func.getName(), "Paulo");
+	}
+	
+	@Test
+	void testandoDataDeNascimento() {
+		assertEquals(func.getDtNascimento(), "11/02/1985");
+	}
+	
+	@Test
+	void testandoCpf() {
+		assertEquals(func.getCpf(), "059.411.417-93");
+	}
+	
+	@Test
+	void testandoNomeDaMae() {
+		assertEquals(func.getNomeMae(), "Lucia");
+	}
+	
+	@Test
+	void testandoNacionalidade() {
+		assertEquals(func.getNacionalidade(), "Brasileiro");
 	}
 
 }
